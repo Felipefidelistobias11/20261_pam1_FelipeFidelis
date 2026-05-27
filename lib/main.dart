78034701174e47c81892da52a3c854383fd67f70
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-05-27 17:09:11 -0300

    continuar na proxima aula

--- lib/main.dart
+++ lib/main.dart
@@ -34,51 +34,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('AppBar'),
-      ),
-      body: Center(
-        child: Text(
-          'Body',
-        style: TextStyle (
-          fontSize: 12,
-          color: Colors.blueGrey,
-          ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.redAccent,
+              child: CircleAvatar(
+                radius: 56,
+                backgroundImage: NetworkImage('https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fi-cant-find-a-bootleg-ralsei-plush-thta-is-on-stock-any-v0-22k3shs3eudf1.jpg%3Fwidth%3D450%26format%3Dpjpg%26auto%3Dwebp%26s%3Dd9914646d4fa203084dd9211e702845f2913d0a5'),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            const Text(
+              'Felipe Fidelis Tobias',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              '16 anos - Mongaguá, SP',
+              style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+            ),
+            const SizedBox(height: 20),
+
+            const Divider(),
+            _buildContactItem(Icons.email, 'felipetec.sp.gov.br'),
+            _buildContactItem(Icons.phone, '(41) 66669-8867'),
+            _buildContactItem(Icons.web, '@felipefidelis'),
+            const Divider(),
+            const SizedBox(height: 20),
+          ],
         ),
-      ),
-      drawer: Drawer(
-        child: Text(  
-          'Drawer',
-        ),
-      ),
-      endDrawer: Drawer(
-        child: Text(
-          'endDrawer'
-          ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'ButtonNavigationBar1',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'ButtonNavigationBar2',
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
-        foregroundColor: Colors.white,
-        onPressed: () {
-          setState(() {
-          });
-        },
-      ),
+      )
     );
   }
 }
